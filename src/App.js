bb2d1edb9633ba3e5461dfe03cf44f68360f7e2b
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-11-08 15:22:44 +0100

    React class component livecoding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("this in constructor", this);
+    this.state = {
+      count: 0,
+      quote: {
+        character: "",
+        image: "",
+        quote: "",
+        characterDirection: "",
+      },
+    };
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  componentDidMount() {
+    console.log("component was mounted");
+    axios
+      .get("https://thesimpsonsquoteapi.glitch.me/quotes")
+      .then((data) => this.setState({ quote: data.data[0] }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    console.log("this was a new render");
+    return (
+      <div className="App">
+        <p>{this.state.count}</p>
+        <button onClick={this.handleIncrement}>INC</button>
+        <p>{this.state.quote.character}</p>
+        <p>{this.state.quote.quote}</p>
+        <img src={this.state.quote.image} />
+      </div>
+    );
+  }
 }
 
 export default App;
